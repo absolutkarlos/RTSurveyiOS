4c6829ba773f19d25a995460c5c93232795f8689
Javier Fonseca <vjfs18@gmail.com>
2016-02-11 18:43:11 -0430

    Maqueta de register (slide) con function basic trabajando.

--- RT Survey/RT Survey/formularioSlideViewController.swift
+++ RT Survey/RT Survey/formularioSlideViewController.swift
@@ -9,92 +9,51 @@
 import Foundation
 import UIKit
 
-class formularioSlideViewController: UIPageViewController, UIPageViewControllerDelegate {
+class formularioSlideViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
-    var index = 0
-    var identifiers: NSArray = ["companyViewController", "contactViewController", "siteViewController", "buildingViewController"]
+    var pages = [UIViewController]()
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         
-        //self.dataSource = self
         self.delegate = self
+        self.dataSource = self
         
-        let startingViewController = self.viewControllerAtIndex(self.index)
-        let viewControllers: NSArray = [startingViewController]
-        self.setViewControllers(viewControllers as? [UIViewController], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
-    }
+        let page1: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("companyN")
+        let page2: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("contactN")
+        let page3: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("siteN")
+        let page4: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("buildingN")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        pages.append(page4)
+
+        
+        setViewControllers([page1], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
     }
     
-    func viewControllerAtIndex(index: Int) -> UIViewController! {
-        
-        if index == 0 {
-            
-            return self.storyboard!.instantiateViewControllerWithIdentifier("companyN")
-            
-        }
-        
-        if index == 1 {
-            
-            return self.storyboard!.instantiateViewControllerWithIdentifier("contactN")
-        }
-        
-        if index == 2 {
-            
-            return self.storyboard!.instantiateViewControllerWithIdentifier("siteN")
-        }
-        
-        if index == 3 {
-            
-            return self.storyboard!.instantiateViewControllerWithIdentifier("buildingN")
-        }
-        return nil
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.indexOf(viewController)!
+        let previousIndex = abs((currentIndex - 1) % pages.count)
+        return pages[previousIndex]
     }
     
-    func pageViewController(pageViewController: UIPageViewController!, viewControllerAfterViewController viewController: UIViewController!) -> UIViewController! {
-        
-        let identifier = viewController.restorationIdentifier
-        let index = self.identifiers.indexOfObject(identifier!)
-        
-        //if the index is the end of the array, return nil since we dont want a view controller after the last one
-        if index == identifiers.count - 1 {
-            
-            return nil
-        }
-        
-        //increment the index to get the viewController after the current index
-        self.index = self.index + 1
-        return self.viewControllerAtIndex(self.index)
-        
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.indexOf(viewController)!
+        let nextIndex = abs((currentIndex + 1) % pages.count)
+        return pages[nextIndex]
     }
     
-    func pageViewController(pageViewController: UIPageViewController!, viewControllerBeforeViewController viewController: UIViewController!) -> UIViewController! {
-        
-        let identifier = viewController.restorationIdentifier
-        let index = self.identifiers.indexOfObject(identifier!)
-        
-        //if the index is 0, return nil since we dont want a view controller before the first one
-        if index == 0 {
-            
-            return nil
-        }
-        
-        //decrement the index to get the viewController before the current one
-        self.index = self.index - 1
-        return self.viewControllerAtIndex(self.index)
-        
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pages.count
     }
     
-    
-    func presentationCountForPageViewController(pageViewController: UIPageViewController!) -> Int {
-        return self.identifiers.count
-    }
-    
-    func presentationIndexForPageViewController(pageViewController: UIPageViewController!) -> Int {
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
         return 0
     }
+
     /*
     // MARK: - Navigation
 
